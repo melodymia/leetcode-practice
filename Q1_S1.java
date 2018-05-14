704854101560cb8cff46415992c56cd4faffcc65
melodymia <39266098+melodymia@users.noreply.github.com>
2018-05-14 22:14:09 +0800

    Create Q1_S1.java
    46ms

---
+++ Q1_S1.java
@@ -0,0 +1,28 @@
+//Two Sum
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result=new int[2];
+        for(int i=0;i<nums.length-1;i++)
+        {
+            for(int j=nums.length-1;j>i;j--)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    result[0]=i;
+                    result[1]=j;
+                }
+                else continue;
+            }
+        }
+        return result;
+    }
+}
+
+/* Description
+nums[0]->nums[length-1],nums[length-2],...,nums[1]
+nums[1]->nums[length-1],nums[length-2],...,nums[2]
+nums[2]->nums[length-1],nums[length-2],...,nums[3]
+...
+*/
+
